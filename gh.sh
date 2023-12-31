df24dc19684bf875295dbf9c029595883d52aaf0
Augusto Brito <augustobritodev@icloud.com>
2023-12-31 20:21:03 -0300

    Update gh.sh

--- gh.sh
+++ gh.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
-
-gh_install() {
-	echo 'GitHub CLI INSTALL'
-	type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 	&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 	&& sudo apt update \
 	&& sudo apt install gh -y
-}
 
-gh_setup() {
-	echo 'GitHub CLI SETUP'
 	gh auth login
 	
 	gh extension install dlvhdr/gh-dash 
@@ -22,5 +15,6 @@ gh_setup() {
 	gh extension install seachicken/gh-poi
 	gh extension install abdfnx/gh-resto 
 	gh extension install gennaro-tedesco/gh-s
-}
+	 gh extension install github/gh-copilot
+
 
